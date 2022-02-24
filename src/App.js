10f2e3cc4a84f222ae2dc69df8389dd91ceb2654
JaeyoonCheon <cjswodbs50@gmail.com>
2022-02-24 17:04:29 +0900

    movie data get from API + genres

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import axios from "axios";
 import Movie from "./Movie";
+import "./App.css";
 
 class App extends React.Component {
   state = {
@@ -23,10 +24,14 @@ class App extends React.Component {
   render() {
     const { isLoading, movies } = this.state;
     return (
-      <div>
-        {isLoading
-          ? "Loadiing..."
-          : movies.map((movie) => {
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader_text">Loading...</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map((movie) => {
               console.log(movie);
               return (
                 <Movie
@@ -35,11 +40,14 @@ class App extends React.Component {
                   year={movie.year}
                   title={movie.title}
                   summary={movie.summary}
-                  poster={movie.poster}
+                  poster={movie.medium_cover_image}
+                  genres={movie.genres}
                 ></Movie>
               );
             })}
-      </div>
+          </div>
+        )}
+      </section>
     );
   }
 }
